77422b14476e2af4a8b3fad802949be7fcec4670
Julio Santacruz <julio.santacruzh@gmail.com>
2024-02-23 09:26:31 -0800

    routing test

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,10 +7,6 @@ export const locales = ['en', 'es'] as const;
 
 export const pathnames = {
   '/': '/',
-  '/pathnames': {
-    en: '/pathnames',
-    es: '/rutas'
-  }
 } satisfies Pathnames<typeof locales>;
 
 // Use the default: `always`
